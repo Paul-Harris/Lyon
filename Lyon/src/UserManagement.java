f2e9f2de1dbce477679e25d22dbfc9c36c23f708
jacob-coleman <jcoleman414@gmail.com>
2014-10-14 16:55:05 -0400

    Added has functions

--- Lyon/src/UserManagement.java
+++ Lyon/src/UserManagement.java
@@ -5,6 +5,27 @@
  * Interactions : Being the controller is pulls data from the database and pushes changes and updates.
  * 
  */
+
+import java.security.*;
+
 public class UserManagement {
+	
+	
+	
+	
+
+	private class PasswordManagement
+	{
+		
+		
+		protected createHash(String pwd)
+		{
+	
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			try { md.update(pwd);
+			return hash
+		}
+		
+	}
 
 }
