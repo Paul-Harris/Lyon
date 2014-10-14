39aae7218e605fc48bfb3e90e3b8dbc2c87a49b6
austinharris <austinharris@10.0.0.9>
2014-10-14 16:51:06 -0400

    UserManagement Changes.

--- Lyon/src/UserManagement.java
+++ Lyon/src/UserManagement.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * @author The Bomb Squad
  * Created : October 14, 2014
@@ -5,6 +7,65 @@
  * Interactions : Being the controller is pulls data from the database and pushes changes and updates.
  * 
  */
-public class UserManagement {
+
+public class UserManagement
+{
+	private Database db;
+	private verify vy;
+	
+	public UserManagement()
+	{
+		
+	}
+	
+	public void command(ArrayList<String> cmd)
+	{
+		String toDo = cmd.get(0);
+		ArrayList<String> nextToDo = cmd.remove(0);
+		switch (toDo)
+		{
+		case "add":
+		//what to do when adding user
+			cmd.remove(0);
+			add(cmd);
+		break;
+		case "delete":
+			cmd.remove(0);
+		//what to do when deleting 
+		break;
+		case  "sign in":
+			cmd.remove(0);
+			
+		//Check verification
+		break;
+		case  "sign up":
+			cmd.remove(0);
+			
+		//Check verification
+		break;
+		}
+		
+		
+	}
+	
+	public void add(ArrayList<String> cmd)
+	{
+		
+	}
+	
+	public void delete(ArrayList<String> cmd)
+	{
+		
+	}
+	
+	public void signIn(ArrayList<String> cmd)
+	{
+		
+	}
+	
+	public void signUp(ArrayList<String> cmd)
+	{
+		
+	}
 
 }
